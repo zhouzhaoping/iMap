806273d62f522cfee29f97517e6246addb6f3cb5
xj6410 <xj6410@gmail.com>
2014-12-20 13:51:52 +0800

    gai bug

--- src/imap/main/MainActivity.java
+++ src/imap/main/MainActivity.java
@@ -831,6 +831,7 @@ public class MainActivity extends Activity {
 				Intent intent = new Intent();
 				intent.setClass(MainActivity.this, imap.rank.RankListActivity.class);
 				startActivity(intent);
+				select_button = -1;
 			}
 		});
 
@@ -841,6 +842,7 @@ public class MainActivity extends Activity {
 				Intent intent = new Intent();
 				intent.setClass(MainActivity.this, imap.setting.SettingActivity.class);
 				startActivity(intent);
+				select_button = -1;
 			}
 		});
 	}
