0b15bc424070e03024e028e8b741a469ce562a34
xj6410 <xj6410@gmail.com>
2014-12-22 14:53:41 +0800

    返回键修改，标签修改

--- src/imap/main/Huatong_dialog.java
+++ src/imap/main/Huatong_dialog.java
@@ -4,9 +4,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 
 import com.example.imap.R;
+
 
 
 
@@ -45,16 +47,19 @@ public class Huatong_dialog extends Activity {
 		recodeTime = bundle.getInt("recodeTime");
 		
 		final RadioGroup sexRadioGroup = (RadioGroup) findViewById(R.id.sex);
-		
-		final CheckBox checkBoxlanguage_c = (CheckBox) findViewById(R.id.chinese);
-		final CheckBox checkBoxlanguage_e = (CheckBox) findViewById(R.id.english);
+		final RadioGroup languageRadioGroup = (RadioGroup) findViewById(R.id.language);
+		final RadioGroup styleRadioGroup = (RadioGroup) findViewById(R.id.style);
+		//final CheckBox checkBoxlanguage_c = (CheckBox) findViewById(R.id.chinese);
+		//final CheckBox checkBoxlanguage_e = (CheckBox) findViewById(R.id.english);
 		final EditText editText_name = (EditText) findViewById(R.id.speak_input);
 		final EditText editText_brief = (EditText) findViewById(R.id.speak_brief_input);
 		Button sureButton  =(Button)findViewById(R.id.sure);	
 		Button cancelButton  =(Button)findViewById(R.id.cancel);
 		Button playButton  =(Button)findViewById(R.id.play);
 		
-		
+		final String []check_sex = {"未知","男生","女生","混合"};
+		final String []check_language = {"未知","中文","英文","方言"};
+		final String []check_sytle = {"其他","严肃","欢快","搞笑"};
 		
 		sureButton.setOnClickListener(new View.OnClickListener() {
 			
@@ -71,14 +76,42 @@ public class Huatong_dialog extends Activity {
 							new FileWriter(file_txt));
 					//添加选择框的值
 					RadioButton sexRadioButton = (RadioButton)findViewById(sexRadioGroup.getCheckedRadioButtonId());
+					RadioButton languageRadioButton = (RadioButton)findViewById(languageRadioGroup.getCheckedRadioButtonId());
+					RadioButton styleRadioButton = (RadioButton)findViewById(styleRadioGroup.getCheckedRadioButtonId());
 					writer.write(editText_name.getText().toString() + "\n");
 					writer.write(editText_brief.getText().toString()+ "\n");
-					writer.write(sexRadioButton.getText().toString()+ "\n");
-					if(checkBoxlanguage_c.isChecked())
+					for(int i = 0 ;i<4;i++)
+					{
+						if(sexRadioButton.getText().toString().equals(check_sex[i]))
+						{
+							//System.out.println(sexRadioButton.getText().toString() +" "+ check_sex[i]);
+							writer.write(i+ "\n");
+						}
+					}
+					
+					for(int i = 0 ;i<4;i++)
+					{
+						if(languageRadioButton.getText().toString().equals(check_language[i]))
+						{
+							//System.out.println(sexRadioButton.getText().toString() +" "+ check_sex[i]);
+							writer.write(i+ "\n");
+						}
+					}
+					for(int i = 0 ;i<4;i++)
+					{
+						if(styleRadioButton.getText().toString().equals(check_sytle[i]))
+						{
+							//System.out.println(sexRadioButton.getText().toString() +" "+ check_sex[i]);
+							writer.write(i+ "\n");
+						}
+					}
+					//writer.write(sexRadioButton.getText().toString()+ "\n");
+					
+				/*	if(checkBoxlanguage_c.isChecked())
 						writer.write(checkBoxlanguage_c.getText().toString()+ "\n");
 					if(checkBoxlanguage_e.isChecked())
 						writer.write(checkBoxlanguage_e.getText().toString()+ "\n");
-					
+				*/	
 					writer.close();
 
 				} catch (Exception e) {
@@ -122,128 +155,4 @@ public class Huatong_dialog extends Activity {
 		
 	}
 }
-		/*final AlertDialog.Builder alert_recorder = new AlertDialog.Builder(this);
-	
-		// 开始弹出对话框,对话框里有播放，确定，取消按钮，
-		// 然后将select=-1，sure_choose_marker = 0;话筒图片还原
-		// 显示弹窗
-		final TableLayout labeLayout = (TableLayout) getLayoutInflater()
-				.inflate(R.layout.label, null);
-		alert_recorder.setView(labeLayout);
-		alert_recorder.setMessage("录音时间：" + ((int) recodeTime));
-
-		// //////////////////////////////////////////////////////////
-
-		alert_recorder.setCancelable(false);
-		alert_recorder.setIcon(R.drawable.huatong);
-		alert_recorder.setTitle("录音结束");
-
 		
-
-		alert_recorder.setPositiveButton("播放",
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-
-						// 设置点击后对话框不删除
-						try {
-							Field field = dialog.getClass().getSuperclass()
-									.getDeclaredField("mShowing");
-							field.setAccessible(true);
-							field.set(dialog, false);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						// 播放语音
-						Intent intent = new Intent();
-						intent.putExtra("view_point_sure_to_update",
-								view_point_sure_to_update);
-						intent.putExtra("timeString", timeString);
-						intent.setClass(Huatong_dialog.this,
-								OneRecorderActivity.class);
-
-						startActivity(intent);
-
-					}
-				});
-		alert_recorder.setNeutralButton("确定",
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-
-						MediaPlayer player = new MediaPlayer();
-						// SimpleDateFormat sDateFormat = new
-						// SimpleDateFormat("yyyyMMddhhmmss");
-						File file = new File(Environment
-								.getExternalStorageDirectory(),
-								"myvoice/voice_" + view_point_sure_to_update
-										+ "_" + timeString + ".amr");
-						try {
-							player.setDataSource(file.getAbsolutePath());
-						} catch (IllegalArgumentException e) {
-							e.printStackTrace();
-						} catch (SecurityException e) {
-							e.printStackTrace();
-						} catch (IllegalStateException e) {
-							e.printStackTrace();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-
-						try {
-							Field field = dialog.getClass().getSuperclass()
-									.getDeclaredField("mShowing");
-							field.setAccessible(true);
-							field.set(dialog, true);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-
-						// 添加标签，生成和voice同名的文档记录
-						File file_txt = new File(Environment
-								.getExternalStorageDirectory(),
-								"myvoice/voice_" + view_point_sure_to_update
-										+ "_" + timeString + ".txt");
-						// write_file();
-
-						EditText log_name = (EditText) labeLayout
-								.findViewById(R.id.log_name);
-						EditText log_content = (EditText) labeLayout
-								.findViewById(R.id.log_content);
-
-						try {
-							BufferedWriter writer = new BufferedWriter(
-									new FileWriter(file_txt));
-
-							writer.write(log_name.getText().toString() + "\n");
-							writer.write(log_content.getText().toString()
-									+ "\n");
-							writer.close();
-
-						} catch (Exception e) {
-
-						}
-					finish();
-					}
-
-				});
-		alert_recorder.setNegativeButton("取消",
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						// 取消，不做操作
-						try {
-							Field field = dialog.getClass().getSuperclass()
-									.getDeclaredField("mShowing");
-							field.setAccessible(true);
-							field.set(dialog, true);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						finish();
-					}
-				});
-		alert_recorder.create().show();
-
-	}*/
-
